2ba1daa98aa18b010f915e6d740c79fe800be08d
Zach Denton <z@chdenton.com>
2024-06-20 17:31:44 -0400

    Add parameters to control joke generation.

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -7,21 +7,42 @@ const model = process.env.OPENAI_MODEL || "gpt-4o";
 
 export const maxDuration = 30;
 
-export async function POST(req: Request) {
+type GenerateJokeParams = {
+  type: string;
+  topic: string;
+};
+
+function jokePrompt({ type, topic }: GenerateJokeParams) {
+  return `Tell me a ${type} joke about ${topic}.`;
+}
+
+function generateJokeResponse(params: GenerateJokeParams) {
   const messages: ChatCompletionMessageParam[] = [
     {
       role: "system",
       content:
-        "Devise a humorous, yet mildly clever one-liner joke suitable for general audiences. Ensure the joke incorporates a bit of surprise or intellectual wit. Do not include any commentary.",
+        "You are the funniest person in the universe. You know how to tell all kinds of jokes. When the user asks for a joke, respond only with the joke. Do not include any commentary.",
+    },
+    {
+      role: "user",
+      content: jokePrompt(params),
     },
   ];
 
-  const response = await openai.chat.completions.create({
+  return openai.chat.completions.create({
     model,
     messages,
     stream: true,
   });
+}
 
+export async function POST(req: Request) {
+  const data = (await req.json())?.data ?? {};
+  const jokeParams = {
+    type: data.type ?? "oneliner",
+    topic: data.topic ?? "programming",
+  };
+  const response = await generateJokeResponse(jokeParams);
   const stream = OpenAIStream(response);
   return new StreamingTextResponse(stream);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,29 +4,53 @@ import * as React from "react";
 import { useChat } from "@ai-sdk/react";
 
 export default function Home() {
-  const { messages, input, reload, isLoading } = useChat({
-    // reload() does nothing if there are no messages.
-    initialMessages: [{ role: "system", content: "unused", id: "unused" }],
-  });
+  const { messages, input, append, isLoading } = useChat();
 
   const handleSubmit = React.useCallback(
     (e: React.FormEvent) => {
       e.preventDefault();
-      reload();
+      append(
+        { role: "system", content: "unused", id: "unused" },
+        {
+          data: Object.fromEntries(new FormData(e.target).entries()),
+        },
+      );
     },
-    [reload],
+    [append],
   );
 
   const jokes = messages.filter((m) => m.role !== "system");
+  const joke = jokes[jokes.length - 1];
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-centter p-24">
-      {jokes.map((m) => (
-        <p key={m.id} className="mb-4">
-          {m.content}
-        </p>
-      ))}
+      {joke && <p className="mb-4">{joke.content}</p>}
       <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="type">Joke type:</label>
+          <select id="type" name="type">
+            <option>oneliner</option>
+            <option>self-deprecating</option>
+            <option>ironic</option>
+            <option>situational</option>
+            <option>observational</option>
+            <option>anecdotal</option>
+            <option>character</option>
+            <option>deadpan</option>
+            <option>farcical</option>
+            <option>slapstick</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="topic">Joke topic:</label>
+          <select id="topic" name="topic">
+            <option>programming</option>
+            <option>AI</option>
+            <option>llamas</option>
+            <option>cheese</option>
+            <option>sports</option>
+          </select>
+        </div>
         <button type="submit">Generate</button>
       </form>
     </main>
